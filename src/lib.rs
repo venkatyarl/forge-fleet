bc72b9ed625c519067bb99d7ca45d1480bb494d3
ForgeFleet <fleet@forgefleet.local>
2026-07-01 03:56:25 -0400

    Migrate core color palette to design tokens
    Automated work_item dispatch (ForgeFleet Pillar 4).

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub mod styles {
+    pub const TOKENS_CSS: &str = include_str!("styles/tokens.css");
+
+    pub const FONT_ASSET_BASE: &str = "/assets/fonts/";
+    pub const INTER_VARIABLE_FONT: &str = "/assets/fonts/Inter-Variable.woff2";
+    pub const JETBRAINS_MONO_VARIABLE_FONT: &str = "/assets/fonts/JetBrainsMono-Variable.woff2";
+
+    pub const FONT_ASSET_PATHS: &[&str] = &[INTER_VARIABLE_FONT, JETBRAINS_MONO_VARIABLE_FONT];
+}
